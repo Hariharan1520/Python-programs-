0723fe8e0c44394b23e84192e2188f2d101c75fa
Hariharan1520 <158480185+Hariharan1520@users.noreply.github.com>
2024-08-04 21:33:12 +0530

    Add files via upload

---
+++ add_function.py
@@ -0,0 +1,6 @@
+def add(a,b):
+    return a+b
+num1=10
+num2=80
+sum=add(num1,num2)
+print(sum)
---
+++ max.py
@@ -0,0 +1,10 @@
+def max(a,b):
+    if(a>b):
+        print(a,"is greater")
+    elif(a==b):
+        print(a,"and",b,"are equal")
+    else:
+        print(b,"is bigger")    
+a=int(input("Enter a number : "))
+b=int(input("Enter a number : "))
+(max(a,b))
